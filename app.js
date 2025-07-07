5ff8dbdba01d452828adc297eeb6f0f135252ec5
MrSurge87 <Mendozsa.sm@gmail.com>
2025-07-07 19:41:03 -0400

    updated cors in index.js

--- app.js
+++ app.js
@@ -25,7 +25,13 @@ app.use((req, res, next) => {
   console.log(`Incoming Request: ${req.method} ${req.url}`);
   next();
 });
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://mrsurge87.github.io",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    crednetials: true,
+  })
+);
 console.log("Middleware Regiestered: cors");
 
 app.use(express.json({ limit: "100mb" }));
